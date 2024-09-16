143871a07c9bc8dae43bf0ce33f1c485bb11e582
brahmjanak14 <83243948+brahmjanak14@users.noreply.github.com>
2024-09-16 16:15:50 +0530

    Add files via upload

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+
+city_name=input("Which city did you grow up in?\n")
+pet_name=input("What is the name of a pet?\n")
+print("Your band name "+city_name+" "+pet_name)
